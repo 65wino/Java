85580bbb2d31870573cc7cef7f65e17bbeab2e5d
65wino <wiktorbrokos@gmail.com>
2026-09-23 16:29:08 +0200

    Add files via upload

---
+++ Brok_zad2.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 1" + "<br>");
+document.write("Pierwszy wiersz to:  16&nbsp;&nbsp;&nbsp;&nbsp;Tak&nbsp;&nbsp;&nbsp;&nbsp;Nie&nbsp;&nbsp;&nbsp;&nbsp;6" + "<br>");
+document.write("Drugi wiersz to: 13&nbsp;&nbsp;&nbsp;&nbsp;Tak&nbsp;&nbsp;&nbsp;&nbsp;Nie&nbsp;&nbsp;&nbsp;&nbsp;6" + "<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("aquamarine").bold().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("navy").bold().fontsize(6)+"<br>"); 
